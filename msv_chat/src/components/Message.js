707bf9e6af5156e43679250a30aab643e44e785a
msv1990 <htconemailbox@gmail.com>
2019-08-08 06:48:39 +0300

    style: change format for date

--- msv_chat/src/components/Message.js
+++ msv_chat/src/components/Message.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Moment from 'react-moment'
 export default ({ from, message , time }) =>
   <p>
-    <strong className="name">{from}</strong> <small><Moment>{time}</Moment></small><br/>
+    <strong className="name">{from}</strong> <small><Moment format="YYYY-MM-DD HH:mm">{time}</Moment></small><br/>
     <br/>
     <em>{message}</em>
   </p>
