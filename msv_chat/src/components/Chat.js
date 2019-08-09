8c60ffa2f7065b17d625d35ee93ecea09da2ad7c
msv1990 <htconemailbox@gmail.com>
2019-08-10 00:40:01 +0300

    feat: add service workers

--- msv_chat/src/components/Chat.js
+++ msv_chat/src/components/Chat.js
@@ -43,17 +43,15 @@ if(localStorage.getItem('user')) {
       if(this.state.rotate && message){
         const options = {
           body: message[0].message,
+          vibrate: [200, 100, 200, 100, 200, 100, 200],
+          tag: message[0].from,
       };
+
       function showNotification() {
         Notification.requestPermission(function(result) {
           if (result === 'granted') {
             navigator.serviceWorker.ready.then(function(registration) {
-              registration.showNotification('Vibration Sample', {
-                body: 'Buzz! Buzz!',
-                icon: '../images/touch/chrome-touch-icon-192x192.png',
-                vibrate: [200, 100, 200, 100, 200, 100, 200],
-                tag: 'vibration-sample'
-              });
+              registration.showNotification(`New message from ${message[0].from}`, options);
             });
           }
         });
